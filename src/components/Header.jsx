a740589e72230f01838c88c5d7b18642fd56be89
xavimaestas <102766454+xavimaestas@users.noreply.github.com>
2023-07-27 15:43:52 -0600

    header created
    header and nav creared

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,13 @@ import { NavLink } from "react-router-dom"
 export default function Header(){
     return (
 
-        <nav>
-           <NavLink to="/">Home</NavLink>
-           <NavLink to="/projects">Projects</NavLink>
-           <NavLink to="/about">About Me</NavLink>
-        </nav>
+        <header>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/projects">Projects</NavLink>
+                <NavLink to="/about">About Me</NavLink>
+            </nav>
+        </header>
+      
     )
 }
