cbac4d69cd01d3381fae1ffd5839a500eb1f913b
xavimaestas <xavimaestas@gmail.com>
2023-08-14 14:41:36 -0600

    home updated
    h1 tags moved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,14 @@ export default function Home(){
             <div className="page home-container">
                 <div className="home-title">
                     <h1>Xavier Maestas</h1> 
+                    <h1>Frontend Developer</h1>
                     <div className="tech-stack">
                         <i className="fa-brands fa-html5 fa-2xl"></i>
                         <i className="fa-brands fa-css3-alt  fa-2xl"></i>
                         <i className="fa-brands fa-js  fa-2xl"></i>
                         <i className="fa-brands fa-react  fa-2xl"></i>
                     </div>
-                    <h1>Frontend Developer</h1>
+                   
                 </div>
 
             
